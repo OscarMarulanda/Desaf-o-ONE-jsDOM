41aaaf19226ba0293e4c89c8eee222bbe720db61
Oscar <odmarulandab@unal.edu.co>
2024-06-28 15:42:19 -0500

    agregar funciones a los botones console y prompt

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 let boton = document.querySelector('h1')
 
 boton.innerHTML = "Hora del Desafío"
+
+function botonConsole(){
+    console.log("El botón fue clickeado")
+}
+
+function brazil(){
+    let ciudad = prompt("Ingresa el nombre de una ciudad en Brazil")
+    alert(`Estuve en ${ciudad} y me acordé de ti`)
+}
